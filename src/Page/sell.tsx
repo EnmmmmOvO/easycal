7aca68f1a383281a8be7954cb944b2477c1c7ec4
EnmmmmOvO <wangjinghan0218@gmail.com>
2024-06-13 17:22:28 +1000

    update ps

--- src/Page/sell.tsx
+++ src/Page/sell.tsx
@@ -30,7 +30,7 @@ const Trading = () => {
   return (
     <Grid container spacing={1} sx={{ height: 100, mt: 0 }}>
       <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center',justifyContent: 'center', mb: 2, mt: 1 }}>
-        <Typography variant="h6">商家销售</Typography>
+        <Typography variant="h6">商家交易计算器</Typography>
       </Grid>
       <Grid item xs={4}>
         <Box sx={{ ml: 1, display: 'flex', alignItems: 'center', height: '100%' }}>
@@ -77,9 +77,13 @@ const Trading = () => {
 
       <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', mb: 2, justifyContent: 'right' }}>
         <Typography variant="body2" sx={{ textAlign: 'right', display: 'block' }}>
-        Gas fee: 0.0001 BNB<br />
-        A$: {audco_aud * 0.0001}
-      </Typography>
+          Gas fee: 0.0001 BNB<br />
+          A$: {audco_aud * 0.0001}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center',justifyContent: 'center', mt: 3 }}>
+        <Typography variant="body2">暂时没有考虑商家提现的3‰的AUDCO，转账DAEX的费用等，未来会考虑进去</Typography>
       </Grid>
 
     </Grid>
