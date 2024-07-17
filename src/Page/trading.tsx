c64734e2e352c8353abf47c32ee2d10fa922a578
harry066266 <162404750+harry066266@users.noreply.github.com>
2024-07-17 12:07:24 +1000

    Update trading.tsx

--- src/Page/trading.tsx
+++ src/Page/trading.tsx
@@ -33,7 +33,197 @@ const Trading = () => {
     );
   }, [total, bnbAmount, audco_usdt, usdt_aud, bnb_usdt, type]);
 
-  return <div></div>;
+  return (
+    <Grid container spacing={2} sx={{ height: 100, mt: 0 }}>
+      <Grid item xs={6}>
+        <FormControl
+          sx={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            multiline
+            value={type}
+            fullWidth
+            label="Type"
+            sx={{
+              "& .MuiSelect-select": {
+                display: "flex",
+                direction: "column",
+                alignItems: "center",
+                borderRadius: 0,
+              },
+            }}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <MenuItem
+              value="0"
+              sx={{
+                display: "flex",
+                direction: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                style={{ height: 20, marginRight: 15 }}
+                src={`${process.env.PUBLIC_URL}/static/aud.svg`}
+                alt={"AUD"}
+              />
+              AUD
+            </MenuItem>
+            <MenuItem
+              value="1"
+              sx={{
+                display: "flex",
+                direction: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                style={{ height: 20, marginRight: 15 }}
+                src={`${process.env.PUBLIC_URL}/static/usd.svg`}
+                alt={"USD"}
+              />
+              USD
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            mr: 2,
+          }}
+        >
+          <img
+            style={{ height: 35, marginRight: 15 }}
+            src={`${process.env.PUBLIC_URL}/static/bnb.svg`}
+            alt={"BNB"}
+          />
+          BNB
+        </Box>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            variant="outlined"
+            label="Total"
+            placeholder="0"
+            onChange={(e) => setTotal(Number(e.target.value))}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            variant="outlined"
+            label="Number"
+            placeholder="0"
+            onChange={(e) => setBnbAmount(Number(e.target.value))}
+          />
+        </Box>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/static/arrow.svg`}
+            alt="arrow"
+            style={{ height: 30, marginTop: 5, transform: "rotate(90deg)" }}
+          />
+        </Box>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <img
+            style={{ height: 35, marginRight: 20 }}
+            src={`${process.env.PUBLIC_URL}/static/audco.jpg`}
+            alt={"AUDCO"}
+          />
+          AUDCO
+        </Box>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          {audcoAmount.toFixed(2)}
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sx={{ mt: 2 }}>
+        <Button
+          fullWidth
+          onClick={() => {
+            setTemp(audcoAmount);
+            setPage(2);
+          }}
+        >
+          {content.calculate}
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Trading;
